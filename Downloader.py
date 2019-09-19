b227f0e08f58a6a27c3e8f0566ef77625d021f25
Alexander-Cairns <alexandergcairns@gmail.com>
2019-09-19 13:51:09 -0300

    Added config.json for storing credentials and download history. Removed Downloader.log. Renamed to Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,46 +1,42 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import json
 
 def get_sesh_id(session):
     soup = BeautifulSoup(session.get('https://moodle31.upei.ca/login/index.php').text,"html.parser")
     return soup.find('input', attrs={'name':'logintoken'})['value']
     
-
-#############################################################
-match = "ecture"
-course_id = "6378"
-username = "acairns2"
-password = 
-#############################################################
+config = json.loads(open('config.json', 'r').read())
 
 session = requests.Session()
-
-
-
 payload = {
     'logintoken': get_sesh_id(session),
-    'username': username,
-    'password': password
+    'username': config["username"],
+    'password': config["password"]
 }
 session.post('https://moodle31.upei.ca/login/index.php', data=payload)
 
-website = session.get("https://moodle31.upei.ca/course/view.php?id=" + course_id)
+website = session.get("https://moodle31.upei.ca/course/view.php?id=" + config["course_id"])
 html = website.text
 # print(html)
 soup = BeautifulSoup(html, "html.parser")
 links = soup.findAll(class_='activityinstance')
 
-logged = open("Downloader.log", 'r')
-downloaded = logged.read()
-log = open("Downloader.log", 'a')
-print(downloaded)
+print('Downloaded:')
+print('===========')
+for name in config["downloaded"]:
+    print(name)
+print('================================================================================')
+print('Downloading:')
+print('===========')
+
 for link in links:
     text = str(link)
-    if match in text:
+    if config["match"] in text:
         quickSoup = BeautifulSoup(text, "html.parser")
         reference = quickSoup.find(class_="instancename").getText().replace(' File', '')
-        if reference not in downloaded:
+        if reference not in config["downloaded"]:
             url = quickSoup.find("a")['href']
             doc = session.get(url)
             disp = doc.headers.get('Content-disposition') 
@@ -48,6 +44,8 @@ for link in links:
             print(name)
             pdf = open( name, 'wb')
             pdf.write(doc.content)
-            log.write(reference)		
-
-
+            # log.write(reference)		
+            config["downloaded"].append(reference)
+config_file = open('config.json', 'w')
+config_file.write(json.dumps(config))
+config_file.close()
