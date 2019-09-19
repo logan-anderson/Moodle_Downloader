5d88c31eb0e865c392e7bed3624b408d6a701fe1
Alexander-Cairns <alexandergcairns@gmail.com>
2019-09-19 16:32:00 -0300

    Rewrote entire program added gitignore

--- Downloader.py
+++ Downloader.py
@@ -6,20 +6,40 @@ import json
 def get_sesh_id(session):
     soup = BeautifulSoup(session.get('https://moodle31.upei.ca/login/index.php').text,"html.parser")
     return soup.find('input', attrs={'name':'logintoken'})['value']
+
+
+
+def login():
+    session = requests.Session()
+    payload = {
+        'logintoken': get_sesh_id(session),
+        'username': config["username"],
+        'password': config["password"]
+    }
+    session.post('https://moodle31.upei.ca/login/index.php', data=payload)
+    return session
+
+def get_moodle_doc(url):
+    doc= session.get(url)
+    disp = doc.headers['Content-disposition']
+    doc_name = re.findall("filename.+", disp)[0].split("\"")[1]
+    return {
+        'doc': doc.content,
+        'name': doc_name
+    }
+
+def write_doc(doc, name):
+    file = open(name, 'wb')
+    file.write(doc)
+    file.close
+
     
 config = json.loads(open('config.json', 'r').read())
 
-session = requests.Session()
-payload = {
-    'logintoken': get_sesh_id(session),
-    'username': config["username"],
-    'password': config["password"]
-}
-session.post('https://moodle31.upei.ca/login/index.php', data=payload)
-
-website = session.get("https://moodle31.upei.ca/course/view.php?id=" + config["course_id"])
+session = login()
+website = session.get("https://moodle31.upei.ca/course/view.php?id=" 
+                       + str(config["course_id"]))
 html = website.text
-# print(html)
 soup = BeautifulSoup(html, "html.parser")
 links = soup.findAll(class_='activityinstance')
 
@@ -32,20 +52,16 @@ print('Downloading:')
 print('===========')
 
 for link in links:
-    text = str(link)
-    if config["match"] in text:
-        quickSoup = BeautifulSoup(text, "html.parser")
-        reference = quickSoup.find(class_="instancename").getText().replace(' File', '')
-        if reference not in config["downloaded"]:
-            url = quickSoup.find("a")['href']
-            doc = session.get(url)
-            disp = doc.headers.get('Content-disposition') 
-            name = re.findall("filename.+", disp)[0].split("\"")[1]
-            print(name)
-            pdf = open( name, 'wb')
-            pdf.write(doc.content)
-            # log.write(reference)		
-            config["downloaded"].append(reference)
+    name = link.find(class_ = "instancename").text
+    if config["match"] not in name.lower() or name in config["downloaded"]:
+        continue
+    print(name)
+    url = link.a["href"]
+    doc = get_moodle_doc(link.a['href'])
+    write_doc(doc['doc'], doc['name'])
+    config["downloaded"].append(name)
+    print('\t', doc['name'])
+
 config_file = open('config.json', 'w')
 config_file.write(json.dumps(config))
 config_file.close()
